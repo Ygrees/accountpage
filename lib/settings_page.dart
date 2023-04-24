905b886b513e4fd60bbea19ef49f4a2e092fff70
Ygrees <sergey.kim12052003@gmail.com>
2023-04-24 16:42:59 +0600

    changed settings

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,163 +18,182 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: Scaffold(
-        backgroundColor: Colors.black, // Установлен голубой цвет фона
-        body: ProfilePage(),
+      home: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        home: SettingsPage(),
       ),
     );
   }
 }
 
+
+
 class ProfileImageAndButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [],
-        ),
-      ],
+    return InkWell(
+      onTap: () {
+        // Your action here
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.info, color: Colors.black, size: 24),
+          SizedBox(width: 8),
+          Text(
+            'Clickable Text',
+            style: TextStyle(color: Colors.black, fontSize: 18),
+          ),
+        ],
+      ),
     );
   }
 }
+
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+        backgroundColor: Colors.white,
         elevation: 0,
-        title: Text(''),
-        iconTheme: IconThemeData(
-          color: Colors.black,
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfilePage()),
-              );
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(30.0),
-          child: Card(
-            elevation: 8,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
+        title: Text('Settings'),
+    iconTheme: IconThemeData(
+    color: Colors.black,
+    ),
+    actions: [
+    IconButton(
+    icon: Icon(Icons.account_circle),
+    onPressed: () {
+    Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => ProfilePage()),
+    );
+    },
+    ),
+    ],
+    ),
+    body: Container(
+    decoration: BoxDecoration(
+    gradient: LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [Colors.white, Colors.white],
+    ),
+    ),
+    child: Center(
+    child: Padding(
+    padding: EdgeInsets.all(30.0),
+    child: Card(
+    elevation: 8,
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(16),
+    ),
+    child: Padding(
+    padding: EdgeInsets.all(16.0),
+    child: ListView(
+    shrinkWrap: true,
+                  children: <Widget>[
+                ProfileImageAndButtons(),
+                SizedBox(height: 10),
+                TextButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.cases_outlined),
+                      Expanded(
+                        child: Text(
+                          'Company Information',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    primary: Colors.black,
+                    textStyle: TextStyle(fontSize: 18),
+                  ),
+                  onPressed: () {},
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.wallet),
+                      Expanded(
+                        child: Text(
+                          'Payment Information',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    primary: Colors.black,
+                    textStyle: TextStyle(fontSize: 18),
+                  ),
+                  onPressed: () {},
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.newspaper),
+                      Expanded(
+                        child: Text(
+                          'Confidential policy and agreement',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    primary: Colors.black,
+                    textStyle: TextStyle(fontSize: 18),
+                  ),
+                  onPressed: () {
+                    // Действия при нажатии на кнопку 3
+                  },
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      Expanded(
+                        child: Text(
+                          'Log Out',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    primary: Colors.black,
+                    textStyle: TextStyle(fontSize: 18),
+                  ),
+                  onPressed: () {
+                    // Действия при нажатии на кнопку "Log Out"
+                     },
+                    ),
+                  ],
+                ),
+              ),
             ),
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  ProfileImageAndButtons(),
-                  SizedBox(height: 10),
-                  TextButton(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.adjust),
-                        Expanded(
-                          child: Text(
-                            '1',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      primary: Colors.black,
-                      textStyle: TextStyle(fontSize: 18),
-                    ),
-                    onPressed: () {},
-                  ),
-                  SizedBox(height: 10),
-                  TextButton(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.adjust),
-                        Expanded(
-                          child: Text(
-                            '2',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      primary: Colors.black,
-                      textStyle: TextStyle(fontSize: 18),
-                    ),
-                    onPressed: () {},
-                  ),
-                  SizedBox(height: 10),
-                  TextButton(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.adjust),
-                        Expanded(
-                          child: Text(
-                            '3',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
-                        style: TextButton.styleFrom(
-    backgroundColor: Colors.transparent,
-    primary: Colors.black,
-    textStyle: TextStyle(fontSize: 18),
-    ),
-    onPressed: () {
-    // Действия при нажатии на кнопку 3
-    },
-    ),
-    SizedBox(height: 10),
-    TextButton(
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-    Icon(Icons.exit_to_app),
-    Expanded(
-    child: Text(
-    'Log Out',
-    textAlign: TextAlign.center,
-    ),
-    ),
-    ],
-    ),
-    style: TextButton.styleFrom(
-    backgroundColor: Colors.transparent,
-    primary: Colors.black,
-    textStyle: TextStyle(fontSize: 18),
-    ),
-    onPressed: () {
-    // Действия при нажатии на кнопку "Log Out"
-    },
-    ),
-    ],
-    ),
-    ),
-    ),
-    ),
-    ),
+          ),
+        ),
+       ),
     );
   }
 }
-
-
-
